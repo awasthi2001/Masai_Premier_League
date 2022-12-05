4459c6fd833a6ce9c3b11ab114835dc14c4264f9
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-05 18:17:11 +0530

    index.js done

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,32 @@
 // You should add submit event on the form
 import { Navbar } from "../Components/Navbar.js";
 
-console.log(Navbar())
 let navbar = document.getElementById("navbar");
 navbar.innerHTML = Navbar()
+
+let form = document.querySelector("#masaiForm");
+
+form.addEventListener('submit',SendFormData);
+let Matches = JSON.parse(localStorage.getItem("scheduling")) || [];
+function SendFormData(e){
+    e.preventDefault();
+    let MatchNumber = document.getElementById("matchNum").value;
+    let TeamA = document.getElementById("teamA").value;
+    let TeamB = document.getElementById("teamB").value;
+    let date = document.getElementById("date").value;
+    let venue = document.getElementById("venue").value;
+   if(MatchNumber=='' || TeamA=='' || TeamB=='' || date=='' || venue==''){
+    alert("please fill all the details");
+   }else{
+    let match = {
+        MatchNumber : MatchNumber,
+        TeamA : TeamA,
+        TeamB : TeamB,
+        date : date,
+        venue : venue
+    }
+    Matches.push(match);
+    localStorage.setItem('scheduling',JSON.stringify(Matches));
+    alert("Successfully Scheduled");
+   }
+}
